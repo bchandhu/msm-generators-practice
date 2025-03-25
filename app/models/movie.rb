e8c9821508314f72e1269b156b47704a84725711
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-03-25 22:14:36 +0000

    worked on sample data

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,8 +13,10 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
-  belongs_to :directors
+  belongs_to :director
   has_many :characters
 
   has_many :cast, :through => :characters, :source=> :actor
+
+  validates(:title, {:presence => true})
 end
