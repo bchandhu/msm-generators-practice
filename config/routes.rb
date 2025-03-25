00a78e0d24b13ad69c4daefe13e151a53ca8d319
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-03-25 21:09:41 +0000

    generated a movie resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
 
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
+  # Routes for the Movie resource:
 
-  # get "/your_first_screen" => "pages#first"
+  # CREATE
+  post("/insert_movie", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
   
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_movie/:path_id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  get("/delete_movie/:path_id", { :controller => "movies", :action => "destroy" })
+
+  #------------------------------
+
 end
